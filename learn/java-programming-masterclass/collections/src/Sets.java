962d4ca5cb70c56336d4dfaecfe95137237370ad
bramwell simiyu <simiyub.engineer@gmail.com>
2020-12-04 09:19:35 +0000

    bulk operations and collection/arrays interaction.

--- learn/java-programming-masterclass/collections/src/Sets.java
+++ learn/java-programming-masterclass/collections/src/Sets.java
@@ -1,6 +1,7 @@
 import data.Dog;
 import data.HeavenlyBody;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -17,6 +18,46 @@ public class Sets {
         printBodies(planets);
 
         Dog dog = new Dog("Twinkle");
+
+        Set<Integer> squares = new HashSet<>();
+        Set<Integer> cubes = new HashSet<>();
+
+        for (int i=1;i <=100;i++){
+            squares.add(i*i);
+            cubes.add(i*i*i);
+        }
+        System.out.println("There are "+squares.size()+ "squares and "+cubes.size()+" cubes");
+
+        /**
+        * Because bulk operations are destructive, we use a new Set here.
+        * */
+        Set <Integer> union = new HashSet<>(squares);
+        union.addAll(cubes);
+        System.out.println("Union contains "+union.size() + " elements.");
+        Set<Integer> intersection = new HashSet<>(squares);
+        intersection.retainAll(cubes);
+        System.out.println("intersection contains"+intersection );
+        for(int i:intersection){
+            System.out.println(i+" is the square of "+Math.sqrt(i)+" and the cube of "+Math.cbrt(i));
+        }
+
+        /**
+         * Collections working with arrays
+         * */
+        Set<String> words = new HashSet<>();
+        String sentence = "one day in the year of the fox";
+        String[] wordArray = sentence.split(" ");
+        words.addAll(Arrays.asList(wordArray));
+        for (String s : words){
+            System.out.println(s);
+        }
+
+        Set <String> nature = new HashSet<>();
+        Set <String> divine = new HashSet<>();
+        String[] natureWords = {"all", "nature", "is", "but", "art", "unknown", "to", "thee"};
+        nature.addAll(Arrays.asList(natureWords));
+        String[] divineWords = {"to", "err", "is", "human", "to", "forgive", "divine"};
+        divine.addAll(Arrays.asList(divineWords));
 
     }
 
