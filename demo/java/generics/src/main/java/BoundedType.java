243e09855f673af69d2295b097ef7c5ce2baeed3
bramwell simiyu <simiyub.engineer@gmail.com>
2021-07-15 09:50:13 +0100

    adding generics

---
+++ demo/java/generics/src/main/java/BoundedType.java
@@ -0,0 +1,21 @@
+
+/**
+ * Want to allow only types of a given super type?
+ * To declare a bounded type parameter, list the type parameter's name,
+ * followed by the extends keyword, followed by its upper bound,
+ * which in this example is Number.
+ * */
+public class BoundedType<T extends Number> {
+    private final T typeT;
+
+    BoundedType(T typeT){
+        this.typeT = typeT;
+            }
+            public T printType(){
+                return typeT;
+            }
+
+    public T genericFunction(T type){
+        return type;
+    }
+}
---
+++ demo/java/generics/src/test/java/MultipleBoundTypeTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultipleBoundTypeTest{
+
+    @Test
+    void printTypePrime(){
+        ANumber aNumber =new ANumber(5);
+        MultipleBoundType<ANumber> multipleBoundType
+                = new MultipleBoundType<>(aNumber);
+        assertEquals(aNumber,multipleBoundType.type());
+        assertTrue(aNumber.divisible(aNumber.number));
+        assertTrue(aNumber.prime(aNumber.number));
+    }
+
+    @Test
+    void printTypeDivisible(){
+        ANumber aNumber4 =new ANumber(4);
+        MultipleBoundType<ANumber> multipleBoundType2
+                = new MultipleBoundType<>(aNumber4);
+        assertEquals(aNumber4,multipleBoundType2.type());
+        assertTrue(aNumber4.divisible(aNumber4.number));
+        assertFalse(aNumber4.prime(aNumber4.number));
+    }
+
+}
