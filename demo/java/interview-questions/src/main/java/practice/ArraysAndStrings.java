af3deadfa7fccdae29d975eee226135bfb091381
bramwell simiyu <simiyub.engineer@gmail.com>
2021-07-27 04:02:13 +0100

    basic logic works

--- demo/java/interview-questions/src/main/java/practice/ArraysAndStrings.java
+++ demo/java/interview-questions/src/main/java/practice/ArraysAndStrings.java
@@ -27,4 +27,50 @@ public class ArraysAndStrings {
 
             return true;
         }
+
+        /**
+         * Consider a string that can contain only characters from a-z.
+         * Write a snippet of code that returns true if this string contains unique characters.
+         * The whitespaces can be ignored.
+         * */
+    public boolean containsUniqueCharactersa_z(String string){
+        Map<Character,Boolean> map = new HashMap<>();
+        for (int i = 0; i < string.length(); i++) {
+            if (string.codePointAt(i) >= 97  && string.codePointAt(i) <= 122) {
+                char c = string.charAt(i);
+                System.out.print(c);
+                if (!Character.isWhitespace(c)) if (map.put(c, true) != null) return false;
+            } else {
+                throw new RuntimeException("Contains character with code value < 97 or >");
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * We can see the binary representation of a number using
+     * Integer.toString(int i, int radix) or Integer.toBinaryString(int i).
+     * Radix represents the base of the number representation.
+     * */
+
+    public String intToBinary(int number, int radix){
+         String bin = Integer.toString(number,radix);
+        System.out.println(bin);
+        return bin;
+
+    }
+
+    /**
+     * We can see the integer representation of a number using
+     * Integer.parseInt(String i, int radix).
+     * Radix represents the base of the number representation.
+     * */
+    public int binaryToInt(String bin, int radix) {
+   return       Integer.parseInt(bin,radix);
+    }
+
+    public int bitwiseUnary(int i) {
+        return ~5;
+    }
 }
--- demo/java/interview-questions/src/test/java/practice/ArraysAndStringsTest.java
+++ demo/java/interview-questions/src/test/java/practice/ArraysAndStringsTest.java
@@ -21,4 +21,33 @@ class ArraysAndStringsTest {
         assertThrows(RuntimeException.class,
                 () ->arraysAndStrings.containsUniqueCharactersASCIIAndUnicode(" \uD83E\uDD2B"));
     }
+
+    @Test
+    void containsUniqueCharactersa_z() {
+        ArraysAndStrings arraysAndStrings = new ArraysAndStrings();
+        assertTrue(arraysAndStrings.containsUniqueCharactersa_z("str"));
+        assertThrows(RuntimeException.class,
+                () ->arraysAndStrings.containsUniqueCharactersa_z("string2"));
+    }
+
+    @Test
+    void intToBinary(){
+        ArraysAndStrings arraysAndStrings = new ArraysAndStrings();
+        int number =  112;
+      //  assertEquals(Integer.toBinaryString(number),arraysAndStrings.intToBinary(number,2));
+        arraysAndStrings.intToBinary(10,2);
+    }
+
+    @Test
+    void binaryToInt(){
+        ArraysAndStrings arraysAndStrings = new ArraysAndStrings();
+        String bin = "1100111";
+        int radix = 2;
+        assertEquals((64+32+4+2+1),arraysAndStrings.binaryToInt(bin, radix));
+    }
+
+    @Test
+    void bitwiseUnary(){
+       assertEquals(2,new ArraysAndStrings().bitwiseUnary(5));
+    }
 }
