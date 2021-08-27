c252d2fc14e78cf14b413d3cf72dda8a9ea2ea00
bramwell simiyu <simiyub.engineer@gmail.com>
2021-08-27 13:21:06 +0100

    cucumber example

---
+++ learn/cucumber/src/test/java/cucumber/StepDefinitions.java
@@ -0,0 +1,43 @@
+package cucumber;
+import io.cucumber.java8.En;
+
+import static org.junit.Assert.*;
+
+public class StepDefinitions implements En {
+
+    private String actualAnswer;
+    private String today;
+
+    public StepDefinitions(){
+
+        Given("today is {string}", (String day) -> {
+            // Write code here that turns the phrase above into concrete actions
+            today = day;
+        });
+
+
+        When("I ask whether it's Friday yet", () -> {
+            // Write code here that turns the phrase above into concrete actions
+           actualAnswer = IsItFriday.isItFriday(today);
+        });
+
+        Then("I should be told {string}", (String expectedAnswer) -> {
+            // Write code here that turns the phrase above into concrete actions
+            assertEquals(expectedAnswer, actualAnswer);
+
+        });
+
+//        Given("today is Friday", () ->{
+//            today = "Friday";
+//        });
+
+
+
+    }
+
+    private static class IsItFriday {
+        public static String isItFriday(String today) {
+            return "Friday".equals(today) ? "TGIF": "Nope";
+        }
+    }
+}
