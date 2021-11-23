09c61058628ef6a50e81776dacc5d00e1a56c81b
bramwell simiyu <simiyub.engineer@gmail.com>
2021-11-23 10:35:11 +0000

    refactored to reuse data and print better

--- interview/src/main/java/SumTwoNumbersInArrayToTargetSumTwoLoopImpl.java
+++ interview/src/main/java/SumTwoNumbersInArrayToTargetSumTwoLoopImpl.java
@@ -1,12 +1,19 @@
-public class SumTwoNumbersInArrayToTargetSumImpl implements SumTwoNumbersInArrayToTargetSum{
+public class SumTwoNumbersInArrayToTargetSumTwoLoopImpl implements SumTwoNumbersInArrayToTargetSum{
+
+    /**
+     * Solution uses two loops: O(n2) Time complexity
+     * */
     public int[] sum(int[] array, int expectedResult) {
         int start = 0;
         if(array.length > 2) {
             while (start <= array.length - 2) {
                 for (int i = start + 1; i <= array.length - 1; i++) {
                     int sum = array[start] + array[i];
-                    if (sum == expectedResult) return new int[]{array[start], array[i]};
-                    System.out.println(array[start] + "," + array[i]);
+                    if (sum == expectedResult) {
+                        System.out.printf("%d + %d = %d %n", array[start], array[i] , expectedResult);
+                        return new int[]{array[start], array[i]};
+                    }
+
                 }
                 start++;
             }
--- interview/src/test/java/SumTwoNumbersInArrayToTargetSumTest.java
+++ interview/src/test/java/SumTwoNumbersInArrayToTargetSumTest.java
@@ -1,30 +1,39 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Array;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SumTwoNumbersInArrayToTargetSumTest {
 
+    private static int target1, target2, target3;
+    private static  int[] array1, array2, array3;
+
     @BeforeEach
     void setUp() {
+        target1 = 10;
+        target2 = 10;
+        target3 = 17;
+        array1 =  new int[]{545,2,1,3,5,1,7,8,9,1,2,3,4,3,5};
+        array2 = new int[]{4,6};
+        array3 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
     }
 
     @Test
-    void sum() {
+    void sumWithTwoLoops() {
 
-        int target = 10;
-        int[] array = new int[]{545,2,1,3,5,1,7,8,9,1,2,3,4,3,5};
-        int[] expectedResult = new SumTwoNumbersInArrayToTargetSumImpl().sum(array, target);
-        assertEquals(target, (expectedResult[0] +expectedResult[1]));
+        int[] expectedResult = new SumTwoNumbersInArrayToTargetSumTwoLoopImpl().sum(array1, target1);
+        assertEquals(target1, (expectedResult[0] +expectedResult[1]));
 
-        int[] array2 = new int[]{4,6};
-        int[] expectedResult2 = new SumTwoNumbersInArrayToTargetSumImpl().sum(array2, target);
-        assertEquals(target, (expectedResult2[0] +expectedResult2[1]));
+        int[] expectedResult2 = new SumTwoNumbersInArrayToTargetSumTwoLoopImpl().sum(array2, target2);
+        assertEquals(target2, (expectedResult2[0] +expectedResult2[1]));
 
-        int[] array3 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] expectedResult3 = new SumTwoNumbersInArrayToTargetSumImpl().sum(array3, 17);
-        assertEquals(17, (expectedResult3[0] +expectedResult3[1]));
+
+        int[] expectedResult3 = new SumTwoNumbersInArrayToTargetSumTwoLoopImpl().sum(array3, target3);
+        assertEquals(target3, (expectedResult3[0] +expectedResult3[1]));
+    }
+
+    @Test
+    void sumWithHashTable(){
+       // int target
     }
 }
