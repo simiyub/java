bd3a8c32ac25d694c059bf90418ea1e658f94e90
bramwell simiyu <simiyub.engineer@gmail.com>
2021-11-23 11:30:17 +0000

    added sum with java sorting

--- interview/src/main/java/SumTwoNumbersInArrayToTargetSumSortImpl.java
+++ interview/src/main/java/SumTwoNumbersInArrayToTargetSumSortImpl.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+
 public class SumTwoNumbersInArrayToTargetSumSortImpl implements SumTwoNumbersInArrayToTargetSum {
     @Override
     public int[] sum(int[] array, int target) {
+        int[] result = Arrays.stream(array).sorted().toArray();
+        System.out.println(Arrays.toString(result));
+        int leftIndex = 0;
+        int rightIndex= result.length-1;
+        for(int i=0;i<result.length-1;i++){
+            if ( result[leftIndex] + result[rightIndex] == target) return new int[] {result[leftIndex], result[rightIndex]};
+            else if (result[leftIndex] + result[rightIndex] < target) leftIndex++;
+            else rightIndex--;
+        }
         return new int[2];
     }
 }
