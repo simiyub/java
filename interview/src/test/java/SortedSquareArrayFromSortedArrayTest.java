fdd7e46d9671abaa11c2bf629589aa717985b01d
bramwell simiyu <simiyub.engineer@gmail.com>
2021-11-23 18:01:22 +0000

    added test that which fails with my implentation as has negative number

--- interview/src/test/java/SortedSquareArrayFromSortedArrayTest.java
+++ interview/src/test/java/SortedSquareArrayFromSortedArrayTest.java
@@ -4,15 +4,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SortedSquareArrayFromSortedArrayTest {
+    private static int[] array1, array2;
+    private static int[] expectedResult1, expectedResult2;
 
     @BeforeEach
     void setUp() {
+        array1 = new int[]{1,2,3};
+        expectedResult1 = new int[]{1,4,9};
+        array2 = new int[]{-3,-2,1};
+        expectedResult2 = new int[] {1,4,9};
     }
 
     @Test
     void sortedSquaredArray() {
-        int[] array = new int[]{1,2,3};
-        int[] expected = new int[]{1,4,9};
-        assertArrayEquals(expected, new SortedSquareArrayFromSortedArrayImpl().sortedSquaredArray(array));
+        assertArrayEquals(expectedResult1, new SortedSquareArrayFromSortedArrayImpl().sortedSquaredArray(array1));
+        assertArrayEquals(expectedResult2, new SortedSquareArrayFromSortedArrayImpl().sortedSquaredArray(array2));
     }
+
+
 }
