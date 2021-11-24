b687c32f37922a291d003a563fda8adfcd4dff24
bramwell simiyu <simiyub.engineer@gmail.com>
2021-11-24 12:41:29 +0000

    added implementation that works for current test cases

--- interview/src/main/java/TournamentWinnerBasicImpl.java
+++ interview/src/main/java/TournamentWinnerBasicImpl.java
@@ -1,10 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TournamentWinnerBasicImpl implements TournamentWinner{
     @Override
     public String tournamentWinner(String[][] matchTeams, int[] results) {
-        String winner = null;
-        for(int i=0;i<matchTeams.length;i++){
-            winner = results[i] == 1 ? matchTeams[0][0] : matchTeams[0][1];
+
+        Map<String,Integer> wins = new HashMap();
+        String currentLeader = null;
+
+        for(int i=0;i<=matchTeams.length-1;i++){
+            String winner = results[i] == 1 ? matchTeams[i][0] : matchTeams[i][1];
+            int currentPoints = wins.getOrDefault(winner, -1);
+            int points = 3;
+            if (currentPoints != -1) {
+                points += currentPoints;
+                wins.replace(winner,points);
+
+            }
+            else{
+                wins.put(winner, points);
+            }
+            if (points > wins.getOrDefault(currentLeader,-1)) currentLeader = winner;
         }
-        return winner;
+
+        return currentLeader;
     }
 }
--- interview/src/test/java/TournamentWinnerTest.java
+++ interview/src/test/java/TournamentWinnerTest.java
@@ -17,5 +17,16 @@ class TournamentWinnerTest {
         String[][] matches=new String[][]{matchOne};
         int[] results = new int[]{1};
         assertEquals(expectedResult, new TournamentWinnerBasicImpl().tournamentWinner(matches,results));
+
+
+        String[][] matches2=new String[][]{new String[]{"team1","team2" }, new String[]{"team1","team3" }};
+        int[] results2 = new int[]{1,1};
+        String expectedResult2="team1";
+        assertEquals(expectedResult2, new TournamentWinnerBasicImpl().tournamentWinner(matches2,results2));
+
+        String[][] matches3=new String[][]{new String[]{"team2","team1" }, new String[]{"team3","team2" }};
+        int[] results3 = new int[]{1,0};
+        String expectedResult3="team2";
+        assertEquals(expectedResult3, new TournamentWinnerBasicImpl().tournamentWinner(matches3,results3));
     }
 }
