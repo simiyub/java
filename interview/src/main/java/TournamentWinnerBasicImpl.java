773cd54f46de6aa68ab2a43d9e937fdf1cebe6b5
bramwell simiyu <simiyub.engineer@gmail.com>
2021-11-24 10:04:38 +0000

    added a bit of logic that would work for one match for the tournament

--- interview/src/main/java/TournamentWinnerBasicImpl.java
+++ interview/src/main/java/TournamentWinnerBasicImpl.java
@@ -1,6 +1,10 @@
 public class TournamentWinnerBasicImpl implements TournamentWinner{
     @Override
     public String tournamentWinner(String[][] matchTeams, int[] results) {
-        return matchTeams[0][0];
+        String winner = null;
+        for(int i=0;i<matchTeams.length;i++){
+            winner = results[i] == 1 ? matchTeams[0][0] : matchTeams[0][1];
+        }
+        return winner;
     }
 }
