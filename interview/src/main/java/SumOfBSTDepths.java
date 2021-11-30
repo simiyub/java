25bd73c6e728835506b9bb8a75105a53ab335439
bramwell simiyu <simiyub.engineer@gmail.com>
2021-11-30 10:01:18 +0000

    added set up for sum of depths question 2

--- interview/src/main/java/SumOfBSTDepths.java
+++ interview/src/main/java/SumOfBSTDepths.java
@@ -1,6 +1,9 @@
 import util.BST;
 import util.Node;
 
+/**
+ * Return the depth of all nodes in a given tree
+ * */
 public interface SumOfBSTDepths {
     int depths(Node tree);
 }
--- interview/src/main/java/SumOfBSTDepthsImpl.java
+++ interview/src/main/java/SumOfBSTDepthsImpl.java
@@ -1,6 +1,9 @@
 import util.BST;
 import util.Node;
 
+/**
+ * Implement the logic for summing depths here
+ * */
 public class SumOfBSTDepthsImpl implements SumOfBSTDepths {
     @Override
     public int depths(Node tree) {
