cf07f1ba93038a2946991d898da2bf96008738c6
simiyub <JlTGvS^5Ly3yLD!ZF5KPM5skOJznAtLcYOr1TnNEG1k8vc#qIT8TkMIxI0w1BquN2&Jju>
2021-11-30 17:07:33 +0000

    added logic for depth first search which works

--- interview/src/main/java/DepthFirstSearchImpl.java
+++ interview/src/main/java/DepthFirstSearchImpl.java
@@ -1,11 +1,23 @@
 import util.BST;
+import util.Node;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class DepthFirstSearchImpl implements DepthFirstSearch{
+public class DepthFirstSearchImpl implements DepthFirstSearch {
 
     @Override
     public List<Integer> list(BST tree) {
-        return null;
+        return listRecursively(tree.getRoot(), new ArrayList<Integer>());
+    }
+
+    private List<Integer> listRecursively(Node node, ArrayList<Integer> nodeNames) {
+        if (node != null) {
+            nodeNames.add(node.getValue());
+            listRecursively(node.getLeft(), nodeNames);
+            listRecursively(node.getRight(), nodeNames);
+        }
+        return nodeNames;
     }
 }
+
