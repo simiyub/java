02b9d81bff010b5f0fcf01d6b8e6d4a1bfc6bffb
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-01 15:12:20 +0000

    remove duplicates from linked list working and passing basic test

--- interview/src/main/java/RemoveDuplicateListImpl.java
+++ interview/src/main/java/RemoveDuplicateListImpl.java
@@ -1,10 +1,23 @@
+/**
+ *
+ * Given a linkedList that is sorted we walk through the list passing through the algorithm
+ * the value of the previous node. If the next node has the same value as previous one then we remove the duplicate.
+ * */
 public class RemoveDuplicateListImpl implements RemoveDuplicatesFromLinkedList {
     @Override
-    public LinkedListRecord removeDuplicates(LinkedListRecord linkedListRecord) {
-        return null;
+    public LinkedListRecord removeDuplicates(LinkedListRecord list) {
+        if(list!=null && list.next!=null){
+            if(list.value == list.next.value) list.next = list.next.next;
+            removeDuplicates(list.next);
+        }
+        return list;
     }
 
     public LinkedListRecord removeDuplicate(LinkedListRecord list) {
-        return null;
+        if(list!=null){
+            if(list.value == list.next.value) list.next = list.next.next;
+            removeDuplicates(list.next);
+        }
+        return list;
     }
 }
--- interview/src/test/java/RemoveDuplicatesFromLinkedListTest.java
+++ interview/src/test/java/RemoveDuplicatesFromLinkedListTest.java
@@ -17,17 +17,17 @@ class RemoveDuplicatesFromLinkedListTest {
         RemoveDuplicatesFromLinkedList.LinkedListRecord two = new RemoveDuplicatesFromLinkedList.LinkedListRecord(2);
         RemoveDuplicatesFromLinkedList.LinkedListRecord one = new RemoveDuplicatesFromLinkedList.LinkedListRecord(1);
         one.next = two;
-        two.next = three;
-        three.next = twoDuplicate;
-        twoDuplicate.next = four;
+        two.next = twoDuplicate;
+        twoDuplicate.next = three;
+        three.next = four;
 
         RemoveDuplicatesFromLinkedList.LinkedListRecord expected = new RemoveDuplicatesFromLinkedList.LinkedListRecord(1);
         RemoveDuplicatesFromLinkedList.LinkedListRecord twoExpected = new RemoveDuplicatesFromLinkedList.LinkedListRecord(2);
         RemoveDuplicatesFromLinkedList.LinkedListRecord threeExpected = new RemoveDuplicatesFromLinkedList.LinkedListRecord(3);
         RemoveDuplicatesFromLinkedList.LinkedListRecord fourExpected = new RemoveDuplicatesFromLinkedList.LinkedListRecord(4);
         expected.next = twoExpected;
-        twoDuplicate.next = threeExpected;
+        twoExpected.next = threeExpected;
         threeExpected.next = fourExpected;
-        assertEquals(expected, new RemoveDuplicateListImpl().removeDuplicate(one));
+        assertEquals(expected, new RemoveDuplicateListImpl().removeDuplicates(one));
     }
 }
