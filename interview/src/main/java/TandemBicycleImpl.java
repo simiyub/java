f5e60b4810eb608e7042823a4906d5002df9e1e8
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-01 12:19:39 +0000

    works as expected

--- interview/src/main/java/TandemBicycleImpl.java
+++ interview/src/main/java/TandemBicycleImpl.java
@@ -1,6 +1,24 @@
+import java.util.Arrays;
+
 public class TandemBicycleImpl implements TandemBicycle{
+
+    private int higher(int first,int second ){
+        return first > second ? first : second;
+    }
+    private int lower(int first, int second){
+        return first < second ? first : second;
+    }
     @Override
     public int speed(int[] teamA, int[] teamB, boolean fastest) {
-        return 0;
+        Arrays.sort(teamA);
+        Arrays.sort(teamB);
+        int speed = 0;
+        int highIndex = teamA.length-1;
+        for(int i=0;i<=teamA.length-1;i++){
+
+            speed += (fastest) ? higher(teamA[i] , teamB[highIndex-i]) : lower(teamA[i] , teamB[i]);
+        }
+        return speed;
+
     }
 }
