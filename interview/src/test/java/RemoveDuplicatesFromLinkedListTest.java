fa3fffb75bb5df0a05319fc51c1f5e3d536d72ae
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-01 15:45:25 +0000

    moving workstations

--- interview/src/test/java/RemoveDuplicatesFromLinkedListTest.java
+++ interview/src/test/java/RemoveDuplicatesFromLinkedListTest.java
@@ -10,7 +10,7 @@ class RemoveDuplicatesFromLinkedListTest {
     }
 
     @Test
-    void removeDuplicates() {
+    void removeDuplicatesTest1() {
         RemoveDuplicatesFromLinkedList.LinkedListRecord four = new RemoveDuplicatesFromLinkedList.LinkedListRecord(4);
         RemoveDuplicatesFromLinkedList.LinkedListRecord twoDuplicate = new RemoveDuplicatesFromLinkedList.LinkedListRecord(2);
         RemoveDuplicatesFromLinkedList.LinkedListRecord three = new RemoveDuplicatesFromLinkedList.LinkedListRecord(3);
@@ -28,6 +28,42 @@ class RemoveDuplicatesFromLinkedListTest {
         expected.next = twoExpected;
         twoExpected.next = threeExpected;
         threeExpected.next = fourExpected;
+        assertEquals(expected, new RemoveDuplicateListImpl().removeDuplicates(one));
+    }
+
+    @Test
+    void removeDuplicatesTest2() {
+        RemoveDuplicatesFromLinkedList.LinkedListRecord six = new RemoveDuplicatesFromLinkedList.LinkedListRecord(6);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord sixDup1 = new RemoveDuplicatesFromLinkedList.LinkedListRecord(6);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord five = new RemoveDuplicatesFromLinkedList.LinkedListRecord(5);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord four = new RemoveDuplicatesFromLinkedList.LinkedListRecord(4);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord fourDup1 = new RemoveDuplicatesFromLinkedList.LinkedListRecord(4);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord fourDup2 = new RemoveDuplicatesFromLinkedList.LinkedListRecord(4);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord fourDup3 = new RemoveDuplicatesFromLinkedList.LinkedListRecord(4);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord three = new RemoveDuplicatesFromLinkedList.LinkedListRecord(3);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord oneDup2 = new RemoveDuplicatesFromLinkedList.LinkedListRecord(1);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord oneDup1 = new RemoveDuplicatesFromLinkedList.LinkedListRecord(1);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord one = new RemoveDuplicatesFromLinkedList.LinkedListRecord(1);
+        one.next = oneDup1;
+        oneDup1.next = oneDup2;
+        oneDup2.next = three;
+        three.next = four;
+        four.next = fourDup1;
+        fourDup1.next = fourDup2;
+        fourDup2.next = fourDup3;
+        fourDup3.next = five;
+        five.next = six;
+        six.next = sixDup1;
+
+        RemoveDuplicatesFromLinkedList.LinkedListRecord expected = new RemoveDuplicatesFromLinkedList.LinkedListRecord(1);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord twoExpected = new RemoveDuplicatesFromLinkedList.LinkedListRecord(2);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord threeExpected = new RemoveDuplicatesFromLinkedList.LinkedListRecord(3);
+        RemoveDuplicatesFromLinkedList.LinkedListRecord fourExpected = new RemoveDuplicatesFromLinkedList.LinkedListRecord(4);
+        expected.next = oneDup1;
+        oneDup2.next = three;
+        three.next = four;
+        four.next = five;
+        five.next = six;
         assertEquals(expected, new RemoveDuplicateListImpl().removeDuplicates(one));
     }
 }
