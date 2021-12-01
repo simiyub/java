b3a98334c8b5c1c7495e2ffe32b6c3101e530c3d
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-01 11:28:01 +0000

    current version works and matches video explanation and passes tests provided.

--- interview/src/main/java/ClassPhoto.java
+++ interview/src/main/java/ClassPhoto.java
@@ -5,6 +5,7 @@
  * 2. Each child in the front row must be shorter than the child in the back row.
  *You are provided with two arrays representing the heights.
  * Determine whether the photo arrangement id feasible or not.
+ * You are allowed to change the arrays.
  *
  * */
 public interface ClassPhoto {
--- interview/src/main/java/ClassPhotoImpl.java
+++ interview/src/main/java/ClassPhotoImpl.java
@@ -1,6 +1,31 @@
+import java.util.Arrays;
+
+/**
+ * O(nlogn) T O(1) S with an iteration through the arrays jointly.
+ * As such O(nlogn).
+ * How it works:
+ * -------------
+ * Greedy algorithm
+ * We sort the arrays so we can compare the arrays jointly.
+ * Then we check if the conditions for the photo are met. Soon we find a conflict, we exit.
+ * */
 public class ClassPhotoImpl implements ClassPhoto {
     @Override
     public boolean feasible(int[] jumper, int[] noJumper) {
-        return false;
+        Arrays.sort(jumper);
+        Arrays.sort(noJumper);
+
+        boolean jumperFront = jumper[0] < noJumper[0] ;
+
+        for (int i = 0; i <= jumper.length - 1; i++) {
+            if (jumperFront) {
+                if(noJumper[i]<=jumper[i]) return false;
+            }
+            else{
+                if(jumper[i] <= noJumper[i]) return false;
+            }
+        }
+        return true;
     }
+
 }
