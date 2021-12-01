5158cb916f82031e923a41a937df1a4225446327
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-01 09:51:19 +0000

    First take

--- interview/src/main/java/MinimumWaitingTimeImpl.java
+++ interview/src/main/java/MinimumWaitingTimeImpl.java
@@ -1,6 +1,27 @@
+import util.BST;
+import util.Node;
+
+import java.util.Arrays;
+
+/**
+ * Go through the array and add up the execution times of previous functions
+ * to determine the waiting time of current function.
+ * */
+
 public class MinimumWaitingTimeImpl implements MinimumWaitingTime{
     @Override
     public int minimumWaitingTime(int[] listOfFunctionExecutionTimes) {
-        return 0;
+        BST tree = new BST();
+        Arrays.stream(listOfFunctionExecutionTimes).forEach(tree::add);
+        return recursivelyCalculateMinimumWaitingTime(tree.getRoot(), 0);
+    }
+
+    private int recursivelyCalculateMinimumWaitingTime(Node node, int executionTime) {
+    if(node!=null){
+        executionTime += node.getValue();
+        recursivelyCalculateMinimumWaitingTime(node.getLeft(), executionTime);
+        recursivelyCalculateMinimumWaitingTime(node.getRight(), executionTime);
+    }
+    return executionTime;
     }
 }
