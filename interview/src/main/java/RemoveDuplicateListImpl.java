7ae826f2308b587f72c80f4052fe9446d1f2cbbf
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-02 09:43:01 +0000

    logic for removing duplicates works

--- interview/src/main/java/RemoveDuplicateListImpl.java
+++ interview/src/main/java/RemoveDuplicateListImpl.java
@@ -1,21 +1,20 @@
 /**
- *
+ * O(n) T and O(1) S as we iterate through the linked list once
  * Given a linkedList that is sorted we walk through the list passing through the algorithm
  * the value of the previous node. If the next node has the same value as previous one then we remove the duplicate.
+ * TODO: Try this with iteration and
+ * TODO: also with an if to walk through the duplicates one at a time rather than all in one go in the while.
  * */
 public class RemoveDuplicateListImpl implements RemoveDuplicatesFromLinkedList {
     @Override
     public LinkedListRecord removeDuplicates(LinkedListRecord list) {
         LinkedListRecord current = list;
-        while(current!=null){
-            if (current.next != null) {
-                LinkedListRecord temp = current.next;
-                if (current.value == current.next.value) {
-                    current.next =temp.next;
-                }
-                    current = current.next;
-            }
 
+        while(current!=null){
+            LinkedListRecord next = current.next;
+            while(next!=null && next.value ==current.value) next = next.next;
+            current.next = next;
+            current = next;
         }
 
         return list;
