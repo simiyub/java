9a46269e1680edae8139b4a944a1e98fcc8d7170
simiyub <JlTGvS^5Ly3yLD!ZF5KPM5skOJznAtLcYOr1TnNEG1k8vc#qIT8TkMIxI0w1BquN2&Jju>
2021-12-02 18:44:05 +0000

    switching workstations

--- interview/src/main/java/BinarySearchImp.java
+++ interview/src/main/java/BinarySearchImp.java
@@ -1,6 +1,16 @@
 public class BinarySearchImp implements BinarySearch{
     @Override
     public int indexOf(int[] array, int requiredNumber) {
-        return 0;
+        int elementCount = array.length-1;
+        return indexOf(array,Math.round(elementCount/2), requiredNumber);
+    }
+
+    private int indexOf(int[] array, int mid, int requiredNumber) {
+
+        mid = Math.round(mid/2);
+
+        if (array[mid]>requiredNumber) return indexOf(array,mid, requiredNumber);
+        else if (array[mid]<requiredNumber) return indexOf(array,mid,requiredNumber);
+        else return mid;
     }
 }
