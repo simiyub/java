270d5cf17d3598937a68cba61206925a505f23bc
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-03 15:32:03 +0000

    changing workstations

--- interview/src/main/java/InsertionSort.java
+++ interview/src/main/java/InsertionSort.java
@@ -1,3 +1,7 @@
+/**
+ * Given an array, return it sorted using the insertion sort algorithm
+ * */
+
 public interface InsertionSort {
     int[] sort(int[] array);
 }
--- interview/src/main/java/InsertionSortImpl.java
+++ interview/src/main/java/InsertionSortImpl.java
@@ -1,6 +1,24 @@
+/**
+ * O(n^2) T in average and worst case as we have to iteratively sort the array through loops to
+ * have the array sorted.
+ * */
+
 public class InsertionSortImpl implements InsertionSort{
     @Override
     public int[] sort(int[] array) {
-        return new int[0];
+        for(int i=1; i<=array.length-2;i++){
+            if(array[i] < array[i-1]) {
+                int endOfSorted = i-1;
+                while(endOfSorted>=0){
+                    if (array[i]<=array[endOfSorted]) {
+                            int oldValue = array[endOfSorted];
+                            array[endOfSorted] = array[i];
+                            array[i] = oldValue;
+                    }
+                    endOfSorted --;
+                }
+            }
+        }
+        return array;
     }
 }
