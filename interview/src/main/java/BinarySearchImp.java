1448a51b941a15d26ed24ff5bd01e597da2ef0b1
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-03 10:01:40 +0000

    first implementation  - recursive works as expected

--- interview/src/main/java/BinarySearchImp.java
+++ interview/src/main/java/BinarySearchImp.java
@@ -2,15 +2,15 @@ public class BinarySearchImp implements BinarySearch{
     @Override
     public int indexOf(int[] array, int requiredNumber) {
         int elementCount = array.length-1;
-        return indexOf(array,Math.round(elementCount/2), requiredNumber);
+        return indexOf(array,0, elementCount, requiredNumber);
     }
 
-    private int indexOf(int[] array, int mid, int requiredNumber) {
+    private int indexOf(int[] array, int start,int end, int requiredNumber) {
 
-        mid = Math.round(mid/2);
+        int mid = Math.round((end+start)/2);
 
-        if (array[mid]>requiredNumber) return indexOf(array,mid, requiredNumber);
-        else if (array[mid]<requiredNumber) return indexOf(array,mid,requiredNumber);
+        if (array[mid]>requiredNumber) return indexOf(array,start, mid-1, requiredNumber);
+        else if (array[mid]<requiredNumber) return indexOf(array,mid+1,end,requiredNumber);
         else return mid;
     }
 }
