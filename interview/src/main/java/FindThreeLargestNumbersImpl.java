f3759e61756c5507d1b14535ebcdd13ba45a2a77
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-03 10:37:48 +0000

    class and failing tests added

---
+++ interview/src/main/java/FindThreeLargestNumbersImpl.java
@@ -0,0 +1,6 @@
+public class FindThreeLargestNumbersImpl implements FindThreeLargestNumbers {
+    @Override
+    public int[] find(int[] array) {
+        return new int[0];
+    }
+}
---
+++ interview/src/test/java/FindThreeLargestNumbersTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindThreeLargestNumbersTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void find() {
+        int[] array = new int[]{1,2, 5, 7, 6, 4, 5,0, 3, 9, 11};
+        int[] expected = new int[]{7,9,11};
+
+        assertArrayEquals(expected, new FindThreeLargestNumbersImpl().find(array));
+    }
+}
