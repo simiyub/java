43502f991a947f9be444c64bcfcee3536337b490
simiyub <JlTGvS^5Ly3yLD!ZF5KPM5skOJznAtLcYOr1TnNEG1k8vc#qIT8TkMIxI0w1BquN2&Jju>
2021-12-06 17:41:54 +0000

    works for basic case

--- interview/src/main/java/CaesarCipherEncryptorImpl.java
+++ interview/src/main/java/CaesarCipherEncryptorImpl.java
@@ -1,9 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /***
  * Using a hashmap to store a mapping that can be used to generate the encrypted string.
  * */
 public class CaesarCipherEncryptorImpl implements CaesarCipherEncryptor {
+
+    private Map<Integer, Character> mapping (int cypherKey){
+        Map<Integer, Character> map = new HashMap<>();
+        char value = 'a';
+        char key = 'a';
+        for (int i=97+cypherKey;i<=122+cypherKey;i++){
+            int index = i;
+            if(i>122) index = i-122;
+            map.put(index,value);
+            value ++;
+        }
+        return map;
+    }
     @Override
     public String encrypt(String string, int shift) {
-        return null;
+        char[] newArray = new char[string.length()];
+        for (int i=0;i<=string.length()-1;i++){
+            int newCode = string.charAt(i)+shift;
+            newArray[i]  = (newCode <= 122) ? (char)newCode : (char)(96+newCode%122);
+        }
+        return new String(newArray);
     }
 }
