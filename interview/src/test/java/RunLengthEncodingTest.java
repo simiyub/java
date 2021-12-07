c5f50f257872f72fb10c126b38b7acf808ea07bf
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-07 14:20:25 +0000

    code works as expected and passed some more edge cases

--- interview/src/test/java/RunLengthEncodingTest.java
+++ interview/src/test/java/RunLengthEncodingTest.java
@@ -5,9 +5,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class RunLengthEncodingTest {
 
     @Test
-    void encode() {
+    void encodeTest1() {
         String expected = "9A4A2B4C1E2D";
         String string = "AAAAAAAAAAAAABBCCCCEDD";
+        assertEquals(expected, new RunLengthEncodingImpl().encode(string));
+    }
+
+    @Test
+    void encodeTest2() {
+        String expected = "1 ";
+        String string = " ";
+        assertEquals(expected, new RunLengthEncodingImpl().encode(string));
+    }
+
+    @Test
+    void encodeTest3() {
+        String expected = "9A9A";
+        String string = "AAAAAAAAAAAAAAAAAA";
+        assertEquals(expected, new RunLengthEncodingImpl().encode(string));
+    }
+
+    @Test
+    void encodeTest4() {
+        String expected = "9;3;9'9'2'111273524142311s";
+        String string = ";;;;;;;;;;;;''''''''''''''''''''1233333332222211112222111s";
         assertEquals(expected, new RunLengthEncodingImpl().encode(string));
     }
 }
