27594bce752954027c0710ae1325cfb14206c06a
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-07 17:03:26 +0000

    works as expected and passes more edge cases

--- interview/src/test/java/GenerateDocumentTest.java
+++ interview/src/test/java/GenerateDocumentTest.java
@@ -5,9 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class GenerateDocumentTest {
 
     @Test
-    void canGenerate() {
-        String characters ="Bste!hetsi ogEAxpelrt x ";
-                String documentRequired = "AlgoExpert is the Best";
+    void canGenerateTest1() {
+        String characters ="When will I see you again ";
+                String documentRequired = "niaga ese Wouy  liwl henI";
         assertTrue(new GenerateDocumentImpl().canGenerate(characters,documentRequired));
+    }
+
+    @Test
+    void canGenerateTest2() {
+        String characters ="When I see you again ";
+        String documentRequired = "niaga ese Wouy  liwl henI";
+        assertFalse(new GenerateDocumentImpl().canGenerate(characters,documentRequired));
+    }
+
+    @Test
+    void canGenerateTest3() {
+        String characters ="When I seeyouagain ";
+        String documentRequired = "niaga ese Wouy  liwl henI";
+        assertFalse(new GenerateDocumentImpl().canGenerate(characters,documentRequired));
     }
 }
