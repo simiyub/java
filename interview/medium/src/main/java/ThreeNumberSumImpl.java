f7758036f1319a34fc362775144cf4336dfb1337
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-08 09:34:39 +0000

    added implementation class and failing unit test

---
+++ interview/medium/src/main/java/ThreeNumberSumImpl.java
@@ -0,0 +1,6 @@
+public class ThreeNumberSumImpl implements ThreeNumberSum {
+    @Override
+    public int[][] sum(int[] array, int targetSum) {
+        return new int[0][];
+    }
+}
---
+++ interview/medium/src/test/java/ThreeNumberSumTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThreeNumberSumTest {
+
+    @Test
+    void sum() {
+        int[][] expected = {new int[]{1,2,3},new int[]{-2,3,5}};
+        int[] array = new int[]{1,2,3,-8,9,-2,3,5,6};
+        assertEquals(expected,new ThreeNumberSumImpl().sum(array,6));
+    }
+}
