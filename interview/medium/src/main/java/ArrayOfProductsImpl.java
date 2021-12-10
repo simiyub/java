c2f5883ff4f1143f1dea027bd773df9ceda8aa63
simiyub <JlTGvS^5Ly3yLD!ZF5KPM5skOJznAtLcYOr1TnNEG1k8vc#qIT8TkMIxI0w1BquN2&Jju>
2021-12-10 18:33:18 +0000

    moving workstations

--- interview/medium/src/main/java/ArrayOfProductsImpl.java
+++ interview/medium/src/main/java/ArrayOfProductsImpl.java
@@ -1,9 +1,18 @@
 /**
- *
+ *O(n) T O(n) S We iterate through the array once by
+ * This function w
  * */
 public class ArrayOfProductsImpl implements ArrayOfProducts{
     @Override
     public int[] products(int[] array) {
+        int[] results = new int[array.length];
+        int leftPointer = 0;
+        int rightPointer = array.length-1;
+        int[] leftResults = new int[];
+        int []rightResults = new int[];
+        while(leftPointer!=rightPointer){
+
+        }
         return new int[0];
     }
 }
