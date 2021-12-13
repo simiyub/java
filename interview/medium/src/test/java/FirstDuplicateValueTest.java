69cf020f268352fd33d939c6e23dcc4c7e8014a5
simiyub <JlTGvS^5Ly3yLD!ZF5KPM5skOJznAtLcYOr1TnNEG1k8vc#qIT8TkMIxI0w1BquN2&Jju>
2021-12-13 15:59:30 +0000

    added edge case and fixed to pass

--- interview/medium/src/test/java/FirstDuplicateValueTest.java
+++ interview/medium/src/test/java/FirstDuplicateValueTest.java
@@ -5,9 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class FirstDuplicateValueTest {
 
     @Test
-    void firstDuplicate() {
+    void firstDuplicate1() {
         int expected = 2;
         int[] array = new int[]{2, 1,5,2,3,3,4};
+
+        assertEquals(expected, new FirstDuplicateValueImpl().firstDuplicate(array));
+    }
+
+    @Test
+    void firstDuplicate2() {
+        int expected = 3;
+        int[] array = new int[]{3, 1, 3, 1, 1, 4, 4};
 
         assertEquals(expected, new FirstDuplicateValueImpl().firstDuplicate(array));
     }
