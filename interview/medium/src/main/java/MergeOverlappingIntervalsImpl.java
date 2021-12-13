14a3e6175185731514a90ce70df709c9a7ee5fad
simiyub <JlTGvS^5Ly3yLD!ZF5KPM5skOJznAtLcYOr1TnNEG1k8vc#qIT8TkMIxI0w1BquN2&Jju>
2021-12-13 17:59:45 +0000

    changing workstations

--- interview/medium/src/main/java/MergeOverlappingIntervalsImpl.java
+++ interview/medium/src/main/java/MergeOverlappingIntervalsImpl.java
@@ -15,6 +15,34 @@
 public class MergeOverlappingIntervalsImpl implements MergeOverlappingIntervals{
     @Override
     public int[][] mergeOverlappingIntervals(int[][] array) {
+        int[][] result = new int[array.length][];
+        boolean useResult = false;
+        int resultIndex=-1;
+        for(int i=0; i<=array.length-2;i++){
+            if(!useResult && array[i][0] <= array[i+1][0] && array[i][0] <=array[i+1][1] && array[i+1][0] <= array[i][1] && array[i+1][1] >= array[i][1]){
+                System.out.printf(""+Math.min(array[i][0], array[i+1][0]), Math.max(array[i][1], array[i+1][1]));
+                result[resultIndex] = new int[]{Math.min(array[i][0], array[i+1][0]), Math.max(array[i][1], array[i+1][1])};
+                useResult = true;
+                continue;
+
+            }
+
+            if(useResult && result[resultIndex][0] <= array[i+1][0] && result[resultIndex][0] <=array[i+1][1] && array[i+1][0] <= result[resultIndex][1] && array[i+1][1] >= result[resultIndex][1]){
+                    System.out.printf(""+Math.min(array[i][0], array[i+1][0]), Math.max(array[i][1], array[i+1][1]));
+                    result[resultIndex] = new int[]{Math.min(result[resultIndex][0], array[i+1][0]), Math.max(result[resultIndex][1], array[i+1][1])};
+                    useResult = true;
+                    continue;
+                }
+            else{
+                resultIndex++;
+                result[resultIndex] = array[i];
+                useResult =false;
+
+            }
+
+
+
+        }
         return new int[0][];
     }
 }
