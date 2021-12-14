b3a539e629b41becb011fb9f58effd441fea3dbf
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-14 12:03:28 +0000

    added ability to insert multiple values, small and big and to check that the values are contained in the tree

--- interview/medium/src/main/java/BST.java
+++ interview/medium/src/main/java/BST.java
@@ -2,13 +2,30 @@
  * BST implementation with O(n) T and O(n) S in worst case
  * */
 public class BST implements BSTContract {
-    private int value;
+    private final int value;
     private BST left;
     private BST right;
 
+    public BST(int value) {
+        this.value =value;
+    }
+
     @Override
     public BST insert(int value) {
-        this.value = value;
+        if(value<this.value) {
+            if(this.left == null) {
+                this.left = new BST(value);
+                return this.left;
+            }
+            return this.left.insert(value);
+        }
+        else if(value>this.value) {
+            if(this.right == null) {
+                this.right = new BST(value);
+                return this.right;
+            }
+            return this.right.insert(value);
+        }
         return this;
     }
 
@@ -20,8 +37,16 @@ public class BST implements BSTContract {
     @Override
     public boolean contains(int value) {
         if(this.value == value) return true;
-        else if (this.left.contains(value)) return true;
-        else return this.right.contains(value);
+        if(value<this.value){
+            if (this.left==null) return false;
+            else return this.left.contains(value);
+
+        }
+        else {
+            if (this.right==null) return false;
+            else return this.right.contains(value);
+
+        }
     }
 
     public int getValue() {
--- interview/medium/src/test/java/BSTTest.java
+++ interview/medium/src/test/java/BSTTest.java
@@ -12,14 +12,63 @@ class BSTTest {
     @Test
     void insert() {
         int expected = 5;
-        BST actual = new BST().insert(5);
+        BST actual = new BST(5);
         assertEquals(expected, actual.getValue());
     }
 
     @Test
+    void insertSmaller() {
+        int expected = 5;
+        BST actual = new BST(5);
+        assertEquals(expected, actual.getValue());
+
+        int smaller = 2;
+        actual.insert(smaller);
+        assertTrue(actual.contains(smaller));
+    }
+
+    @Test
+    void insertBigger() {
+        int expected = 2;
+        BST actual = new BST(2);
+        assertEquals(expected, actual.getValue());
+
+        int bigger = 5;
+        actual.insert(bigger);
+        assertTrue(actual.contains(bigger));
+    }
+
+
+    @Test
+    void insertMultiple() {
+        int expected = 2;
+        BST actual = new BST(2);
+        assertEquals(expected, actual.getValue());
+
+        int five = 5;
+        actual.insert(five);
+        assertTrue(actual.contains(five));
+
+        int one = 1;
+        actual.insert(one);
+        assertTrue(actual.contains(one));
+
+        int nine = 9;
+        actual.insert(nine);
+        assertTrue(actual.contains(nine));
+
+        int three = 3;
+        actual.insert(three);
+        assertTrue(actual.contains(three));
+    }
+
+
+
+    @Test
     void remove() {
+        int root = 1;
         int value = 4;
-        BST bst = new BST();
+        BST bst = new BST(root);
         bst.insert(value);
         assertTrue(bst.contains(value));
         bst.remove(value);
@@ -29,7 +78,7 @@ class BSTTest {
     @Test
     void contains() {
         int value = 3;
-        BST bst = new BST();
+        BST bst = new BST(1);
         bst.insert(value);
         assertTrue(bst.contains(value));
     }
