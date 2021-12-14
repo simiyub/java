317f4babf55ff8db38e72b106bf676a4be175896
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-14 10:15:57 +0000

    moved files to a package and added more documentation

--- interview/medium/src/main/java/mergeoverlap/MergeOverlappingIntervalsImpl.java
+++ interview/medium/src/main/java/mergeoverlap/MergeOverlappingIntervalsImpl.java
@@ -4,17 +4,17 @@ import java.util.Comparator;
 import java.util.List;
 
 /***
- * O(n) T O(1) S as we iterate through the array
+ * O(n) T O(1) S as we iterate use the same array
  * identifying overlapping intervals and updating the array in place and returning the array once we are at the end.
  *
  * How it works
  * ------------
+ * First we sort the array by first item in each interval
  * My solution considers the following conditions and merges arrays accordingly:
  * Intervals are considered overlapping when:
  *  * considering this array: [[x,y],[a,b]]
  *  * x is less than a and b
  *  * a is less than or equal to y
- *  * b id greater than or equal to y.
  * */
 
 public class MergeOverlappingIntervalsImpl implements MergeOverlappingIntervals{
