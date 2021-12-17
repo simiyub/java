c96c5ef14bad70467791be83e0dd004518076c90
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-17 12:53:56 +0000

    works as expected and passes basic test and algoexpert.io tests

--- interview/medium/src/main/java/FindKthLargestValueInBSTImpl.java
+++ interview/medium/src/main/java/FindKthLargestValueInBSTImpl.java
@@ -1,5 +1,9 @@
 /**
- * Immediate approach that comes to mind which would be O(n) T (n) S is:
+ * Immediate approach that comes to mind which would be O(h+k) T (h) S. This is because
+ * we will go as far down as the height of the tree as it takes to arrive atk
+ * We will be running this recursively and storing the values in the stack thus needing h for space
+ * How it works
+ * ------------
  * Find the largest value in the tree.
  * start a counter
  * check if the counter = k
@@ -15,16 +19,31 @@
  * we reach the parent where we started
  * At this point, we go down left one level and repeat traversal down the right, and then
  * walking back up the tree
- * We continue with this approach until we get to the kth largest element or we've inspected all
+ * We continue with this approach until we get to the kth largest element, or we've inspected all
  * nodes and none is the kth largest
  *
  * */
 
 
 public class FindKthLargestValueInBSTImpl implements FindKthLargestValueInBST{
+
+    int kthLargest = -1;
+    int recordCount = 0;
+    boolean notFound = true;
     @Override
     public int kthLargestValue(BST bst, int k) {
 
-        return 0;
+        if(bst!=null && notFound){
+            kthLargestValue(bst.right, k);
+            recordCount +=1;
+            if (recordCount == k) {
+                notFound = false;
+                kthLargest =  bst.value;
+            }
+            kthLargestValue(bst.left, k);
+
+        }
+
+        return kthLargest;
     }
 }
