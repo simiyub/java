e17031ba90733ce345eacf4d4318f7246b44d426
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-17 11:25:34 +0000

    refactored and simplified to a more efficient version

--- interview/medium/src/main/java/MinHeightImpl.java
+++ interview/medium/src/main/java/MinHeightImpl.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 /**
- * O(nlogn) T O(n) S will go through the array starting from the middle
+ * O(n) T O(n) S will go through the array starting from the middle
  * and then splitting the array into two each time to find the value that is less than
  *  the middle value and the one that is larger and adding the bst onto the tree for
  *  each of these cases and recursively doing this for all entries in the array.
@@ -10,19 +10,16 @@ public class MinHeightImpl implements MinHeightBST{
     @Override
     public BST minHeightBST(List<Integer> array) {
 
-        return minHeightBST(array,null, 0, array.size()-1);
+        return minHeightBST(array, 0, array.size()-1);
     }
 
-    private BST minHeightBST(List<Integer> array, BST bst, int startIndex, int endIndex) {
-        int index = Math.round((startIndex+endIndex) / 2);
-
-        if (startIndex<=endIndex) {
-            if (bst == null) bst = new BST(array.get(index));
-            else bst.insert(array.get(index));
-
-            minHeightBST(array,bst,startIndex,index-1);
-            minHeightBST(array,bst,index+1,endIndex );
-
+    private BST minHeightBST(List<Integer> array,int startIndex, int endIndex) {
+        BST bst = null;
+        if(startIndex<=endIndex){
+        int index = (startIndex+endIndex) / 2;
+         bst = new BST(array.get(index));
+        bst.left = minHeightBST(array,startIndex,index-1);
+        bst.right = minHeightBST(array,index+1,endIndex );
         }
 
         return bst;
