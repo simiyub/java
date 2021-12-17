180102c194eac64385d5688dffbd229b040d519b
bramwell simiyu <simiyub.engineer@gmail.com>
2021-12-17 12:09:23 +0000

    added documentation on my approach to meeting the requirement

--- interview/medium/src/main/java/FindKthLargestValueInBSTImpl.java
+++ interview/medium/src/main/java/FindKthLargestValueInBSTImpl.java
@@ -1,6 +1,30 @@
+/**
+ * Immediate approach that comes to mind which would be O(n) T (n) S is:
+ * Find the largest value in the tree.
+ * start a counter
+ * check if the counter = k
+ * if not, then move on to the parent of the largest value and increment the counter
+ * check if the counter = k
+ * if counter not = k
+ * go to the left of the parent incrementing the counter.
+ * If we are still not onto k, we move on to the parent
+ * We repeat this process until we get to the root, assuming we are not on the kth largest
+ * We go down the left of the tree one level then go all the way down right to the
+ * bottom of the branch.
+ * We then repeat the process we did on the right branch earlier and walk up the tree till
+ * we reach the parent where we started
+ * At this point, we go down left one level and repeat traversal down the right, and then
+ * walking back up the tree
+ * We continue with this approach until we get to the kth largest element or we've inspected all
+ * nodes and none is the kth largest
+ *
+ * */
+
+
 public class FindKthLargestValueInBSTImpl implements FindKthLargestValueInBST{
     @Override
     public int kthLargestValue(BST bst, int k) {
+
         return 0;
     }
 }
