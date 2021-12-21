8b7844937a70f06509bd48fabaa472f58a5449b8
simiyub <JlTGvS^5Ly3yLD!ZF5KPM5skOJznAtLcYOr1TnNEG1k8vc#qIT8TkMIxI0w1BquN2&Jju>
2021-12-21 12:28:45 +0100

    added logic to be used before checking algoexpert.io for hints

--- interview/medium/src/main/java/HeightBalancedBinaryTreeImpl.java
+++ interview/medium/src/main/java/HeightBalancedBinaryTreeImpl.java
@@ -1,3 +1,13 @@
+/**
+ * O(n) T and O(1) S in worst case scenario as we will iterate through the tree comparing the left and right branches,
+ * comparing the depth of each without storing any values until we find a difference greater than one height.
+ *
+ * How it works
+ * ------------
+ * We iterate through the tree, comparing the depth of the left branch to the height of the
+ * right branch. Soon as we find a difference more than 1, we break and return false. Otherwise
+ * we continue comparing to the end of the tree and return true.
+ * */
 public class HeightBalancedBinaryTreeImpl implements HeightBalancedBinaryTree{
     @Override
     public boolean isBalanced(BinaryTree tree) {
