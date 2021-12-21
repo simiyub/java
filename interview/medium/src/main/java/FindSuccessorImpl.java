987652324937da61a001bf24cb5bc67ad0233ded
simiyub <JlTGvS^5Ly3yLD!ZF5KPM5skOJznAtLcYOr1TnNEG1k8vc#qIT8TkMIxI0w1BquN2&Jju>
2021-12-21 11:54:39 +0100

    works as expected and passes unit test

--- interview/medium/src/main/java/FindSuccessorImpl.java
+++ interview/medium/src/main/java/FindSuccessorImpl.java
@@ -1,6 +1,32 @@
+/**
+ * O(h) T on average and O(1) S but could go to O(n) T if the tree only has one sided branch
+ * such as only left children or only right children.
+ * This function uses the logic explained in the algoexpert.io solution of the find successor.
+ * The successor of the node will be the most left child of the node's right branch or it's ancestor if there
+ *  is no left node.
+ *  So we traverse the tree and if there is a right branch, we find the most left
+ *  node of the right branch. If it doesn't exist, then we find the ancestor of the node.
+ * */
+
 public class FindSuccessorImpl implements FindSuccessor{
     @Override
     public BinaryTree successor(BinaryTree tree, BinaryTree node) {
+        if (tree!=null){
+            if(node.right!=null) return mostLeftChild(node.right);
+            return ancestor(node);
+        }
         return null;
+    }
+
+    private BinaryTree ancestor(BinaryTree node) {
+        BinaryTree currentNode = node;
+        while (currentNode.parent!=null && currentNode.parent.right==currentNode) currentNode = currentNode.parent;
+        return currentNode.parent;
+    }
+
+    private BinaryTree mostLeftChild(BinaryTree node) {
+        BinaryTree currentNode=node;
+        while(currentNode.left!=null) currentNode = currentNode.left;
+        return currentNode;
     }
 }
--- interview/medium/src/test/java/FindSuccessorTest.java
+++ interview/medium/src/test/java/FindSuccessorTest.java
@@ -6,9 +6,30 @@ class FindSuccessorTest {
 
     @Test
     void testSuccessor(){
-        int expected = 0;
-        FindSuccessor.BinaryTree tree = new FindSuccessor.BinaryTree(3);
-        assertEquals(expected, new FindSuccessorImpl().successor(tree,tree));
+        int expected = 1;
+        FindSuccessor.BinaryTree six =  new FindSuccessor.BinaryTree(6);
+        FindSuccessor.BinaryTree two = new FindSuccessor.BinaryTree(2);
+        FindSuccessor.BinaryTree four = new FindSuccessor.BinaryTree(4);
+        FindSuccessor.BinaryTree five = new FindSuccessor.BinaryTree(5);
+        FindSuccessor.BinaryTree three = new FindSuccessor.BinaryTree(3);
+
+        FindSuccessor.BinaryTree tree = new FindSuccessor.BinaryTree(1);
+        five.parent = two;
+        six.parent = four;
+
+        four.left = six;
+        four.parent = two;
+
+        two.left = four;
+        two.right = five;
+        two.parent = tree;
+
+        three.parent = tree;
+
+        tree.left = two;
+        tree.right = three;
+        FindSuccessor.BinaryTree node = five;
+        assertEquals(expected, new FindSuccessorImpl().successor(tree,node).value);
     }
 
 }
