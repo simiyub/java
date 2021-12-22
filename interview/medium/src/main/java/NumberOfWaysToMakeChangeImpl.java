92b858d0c39fadf20e7e488590b8b39db008b2a5
simiyub <JlTGvS^5Ly3yLD!ZF5KPM5skOJznAtLcYOr1TnNEG1k8vc#qIT8TkMIxI0w1BquN2&Jju>
2021-12-22 11:05:47 +0100

    works as expected and passes a unit test

--- interview/medium/src/main/java/NumberOfWaysToMakeChangeImpl.java
+++ interview/medium/src/main/java/NumberOfWaysToMakeChangeImpl.java
@@ -4,6 +4,20 @@
 public class NumberOfWaysToMakeChangeImpl implements NumberOfWaysToMakeChange{
     @Override
     public int numberOfWaysToMakeChange(int[] array,int desiredAmount) {
-        return 0;
+        int count = 0;
+        int plausibleCombinations=0;
+        int additionCombinationIndex = 0;
+        for(int i=0;i<=array.length-1;i++){
+            if(array[i]<=desiredAmount) {
+                additionCombinationIndex = i+1;
+                count += desiredAmount % array[i] == 0 ? 1 : 0;
+                while(additionCombinationIndex<=array.length-1){
+                    count += desiredAmount % (array[i]+array[additionCombinationIndex]) == 0 ? 1 : 0;
+                    count += desiredAmount % (array[i]*array[additionCombinationIndex]) == 0 ? 1 : 0;
+                    additionCombinationIndex++;
+                }
+            }
+        }
+        return count;
     }
 }
