59e43338ac2d1c6e632be75c6b984c23fff4b482
bramwell simiyu <simiyub.engineer@gmail.com>
2022-01-07 18:03:51 +0000

    fixed failing edge case

--- interview/medium/src/main/java/LongestPalindromicSubstringImpl.java
+++ interview/medium/src/main/java/LongestPalindromicSubstringImpl.java
@@ -23,8 +23,8 @@ public class LongestPalindromicSubstringImpl implements LongestPalindromicSubstr
             for (int i = 0; i <= string.length()-2; i++) {
                 boolean b = i>0 && string.charAt(i - 1) == string.charAt(i + 1);
                 boolean b1 = string.charAt(i) == string.charAt(i + 1);
-                if (b1 || b) {
-                    int leftIndex = b ? i-1 : i;
+                if (b1) {
+                    int leftIndex =  i;
                     int rightIndex = i+1;
                     boolean canExtend =true;
 
@@ -41,6 +41,27 @@ public class LongestPalindromicSubstringImpl implements LongestPalindromicSubstr
                     if((rightIndex-leftIndex)>(rightIndexLongest-leftIndexLongest)){
                      leftIndexLongest = leftIndex;
                      rightIndexLongest = rightIndex;
+                    }
+                }
+
+                if (b) {
+                    int leftIndex = i-1;
+                    int rightIndex = i+1;
+                    boolean canExtend =true;
+
+                    while (canExtend ){
+                        if (leftIndex-1 >=0 && rightIndex+1<=string.length()-1 &&
+                                string.charAt(leftIndex-1) == string.charAt(rightIndex+1)) {
+                            canExtend = true;
+                            leftIndex--;
+                            rightIndex++;
+                        }
+                        else canExtend = false;
+                    }
+
+                    if((rightIndex-leftIndex)>(rightIndexLongest-leftIndexLongest)){
+                        leftIndexLongest = leftIndex;
+                        rightIndexLongest = rightIndex;
                     }
                 }
             }
