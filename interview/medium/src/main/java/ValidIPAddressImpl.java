10b73154c42030c027d58bbc292cd659bf19e007
bramwell simiyu <simiyub.engineer@gmail.com>
2022-01-19 11:35:32 +0000

    created on the back of the algoexpert suggestion - works and passes unit tests

--- interview/medium/src/main/java/ValidIPAddressImpl.java
+++ interview/medium/src/main/java/ValidIPAddressImpl.java
@@ -11,12 +11,57 @@ import java.util.List;
  * When we find a substring of the string that is valid for each of the four parts we add
  * it to the valid ip address List and repeat until we have run through the whole string
  * and return the result.
+ * TODO Simplify this
  * */
 public class ValidIPAddressImpl implements ValidIPAddress {
+
     @Override
-    public String[] validIPAddresses(String values) {
+    public String[] validIPAddresses(String string){
+        if (string.length()>12 || string.startsWith("0")) return new String[0];
         List<String> ipAddresses = new ArrayList<>();
-        //st
+        for(int i=1;i<Math.min(string.length(), 4);i++){
+
+            String[] currentIPAddress = new String[]{"","","",""};
+            String first = string.substring(0,i);
+            if(isValid(first)){
+                currentIPAddress[0] = first;
+
+                for (int j=i+1;j<i+Math.min(string.length()-i,4);j++){
+                String second= string.substring(i,j);
+                if (isValid(second)) {
+                    currentIPAddress[1] = second;
+
+                    for (int k = j + 1; k < j + Math.min(string.length() - j, 4); k++) {
+                        String third= string.substring(j, k);
+                        String fourth = string.substring(k);
+
+                        if (isValid(third) && isValid(fourth)) {
+                            currentIPAddress[2] = third;
+                            currentIPAddress[3] = fourth;
+                            ipAddresses.add(arrayAsString(currentIPAddress));
+                        }
+                    }
+                }
+            }}
+
+
+        }
         return ipAddresses.toArray(new String[0]);
+    }
+
+    private String arrayAsString(String[] array) {
+        StringBuilder builder = new StringBuilder();
+        for (int i=0;i< array.length;i++){
+            builder.append(array[i]);
+            if(i<array.length-1)builder.append(".");
+        }
+        return builder.toString();
+    }
+
+    private boolean isValid(String current) {
+        int intValue = Integer.parseInt(current);
+        boolean isValidInt = Integer.toString(intValue).equals(current);
+        boolean isValidIPInt = intValue<=255;
+        return isValidInt && isValidIPInt;
     }
 }
--- interview/medium/src/test/java/ValidIPAddressTest.java
+++ interview/medium/src/test/java/ValidIPAddressTest.java
@@ -5,19 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class ValidIPAddressTest {
 
     @Test
-    void validIPAddresses() {
+    void testValidIPAddresses0() {
+
+        String[] expected = new String[0];
+        String[] actual = new ValidIPAddressImpl().validIPAddresses("01921680");
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    void testValidIPAddresses1() {
 
         String[] expected = new String[]{
                 "1.9.216.80",
                 "1.92.16.80",
                 "1.92.168.0",
-                "19.2.16,80",
+                "19.2.16.80",
                 "19.2.168.0",
                 "19.21.6.80",
                 "19.21.68.0",
                 "19.216.8.0",
-                "192.1.68.0",
                 "192.1.6.80",
+                "192.1.68.0",
                 "192.16.8.0"
         };
         String[] actual = new ValidIPAddressImpl().validIPAddresses("1921680");
